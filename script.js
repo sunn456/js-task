3e48586b2fe51fcb010f0f05792ca19080b18b27
sunn456 <mariakulyk05@gmail.com>
2025-09-20 19:03:05 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+alert('JS connrcted');
+const userName = prompt("Enter your name:");
+const userAgeInput = prompt("Enter your age:");
+
+const userAge = Number(userAgeInput);
+
+const user = { name: userName, age: userAge };
+
+console.log(`Name: ${user.name}, Age: ${user.age}`);
+
+if (Number.isNaN(user.age)) {
+  console.log("Invalid age input");
+} else if (user.age < 18) {
+  console.log("Access denied");
+} else {
+  console.log("Access granted");
+}
